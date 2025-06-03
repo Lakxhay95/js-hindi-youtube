27f1c135b65cef5ae8fb53c22ce3dbcc676db85e
Lakxhay95 <glakshay687@gmail.com>
2025-06-03 10:23:56 +0000

    about strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,7 @@
+const name = "Lakshay"
+const repoCount = 3
+console.log(`Hello my name is ${name} and I have a repository count of ${repoCount}`)
+
+// Another way of initilizing string
+const anotherName = new String("Gupta")
+console.log(`${name} ${anotherName}`)
