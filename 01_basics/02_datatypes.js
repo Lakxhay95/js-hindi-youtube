0c06f35d22e10eddab1c3ad829285c39cb209f3e
Lakxhay95 <glakshay687@gmail.com>
2025-06-03 06:40:48 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict" // treat all js code as new version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    + 
+    3) // Code readiblity should be high
+console.log("Lakshay")
+
+let name = "lakshay"
+let age = 22
+let isLoggedIn = true
+let state
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean = true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof null); // object
+console.log(typeof state); // undefined
