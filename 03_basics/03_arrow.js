4f5faa6885f6918a57c4fbfd722d8046d2dfbdae
Lakxhay95 <glakshay687@gmail.com>
2025-06-04 10:52:31 +0000

    arrow function

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,9 @@
+// arrow functions
+const example = (num1, num2) => {
+    return num1 + num2
+}
+console.log(example(6, 4))
+
+// Example of implicit return
+const add = (num1, num2) => num1 + num2
+console.log(add(10, 20))
